7f2704fd94fdcb32306a55d9fe5f1a031701f24d
Jaidev Bangar <solaxes@gmail.com>
2024-03-26 20:15:28 +0530

    parcel module installation

--- 02-firstapp/app.js
+++ 02-firstapp/app.js
@@ -1,8 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
 // Create Element here takes three arguments
 const heading = React.createElement("div", { id: "div1" }, [
   React.createElement("div", { id: "dev1" }, [
-    React.createElement("h1", { id: "heading1" }, "Heading 1"),
-    React.createElement("h2", { id: "heading2" }, "heading 2"),
+    React.createElement("h1", { id: "heading1" }, "Jaidev Bangar"),
+    React.createElement("h2", { id: "heading2" }, "Hot module replacement  🚀"),
   ]),
   React.createElement("div", { id: "div2" }, [
     React.createElement("h1", { id: "heading1" }, "Heading 1"),
---
+++ 02-firstapp/dist/index.7c0ccee6.js
@@ -0,0 +1,29 @@
+// Create Element here takes three arguments
+const heading = React.createElement("div", {
+    id: "div1"
+}, [
+    React.createElement("div", {
+        id: "dev1"
+    }, [
+        React.createElement("h1", {
+            id: "heading1"
+        }, "Heading 1"),
+        React.createElement("h2", {
+            id: "heading2"
+        }, "heading 2")
+    ]),
+    React.createElement("div", {
+        id: "div2"
+    }, [
+        React.createElement("h1", {
+            id: "heading1"
+        }, "Heading 1"),
+        React.createElement("h2", {
+            id: "heading2"
+        }, "Heading 2")
+    ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
