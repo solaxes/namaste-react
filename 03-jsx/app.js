da7bdb7a18d0d5ed0723fbfbcfc617fca8ed41d4
Jaidev Bangar <solaxes@gmail.com>
2024-03-27 00:12:10 +0530

    Final commit for jsx

--- 03-jsx/app.js
+++ 03-jsx/app.js
@@ -2,13 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 // Create Element here takes three arguments
 
+const reactElement = (
+  <div>
+    <h1>This is React Element</h1>
+    <p>This is paragraph</p>
+  </div>
+);
+
 const TitleComponent = () => {
   return <h1>This is my Title</h1>;
 };
 
+const number = 1000;
+
 const HeadingComponent = () => {
   return (
     <>
+      {reactElement}
+      {number}
       <TitleComponent />
       <h1>Hello from inside the functional component</h1>
       <p>Hello </p>
