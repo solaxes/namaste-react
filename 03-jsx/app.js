88bf2bae4fae2fc09ada88ae193dad35fee441c3
Jaidev Bangar <solaxes@gmail.com>
2024-03-26 22:07:22 +0530

    Used JSX first time

--- 03-jsx/app.js
+++ 03-jsx/app.js
@@ -1,16 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // Create Element here takes three arguments
-const heading = React.createElement("div", { id: "div1" }, [
-  React.createElement("div", { id: "dev1" }, [
-    React.createElement("h1", { id: "heading1" }, "Jaidev Bangar"),
-    React.createElement("h2", { id: "heading2" }, "Hot module replacement  🚀"),
-  ]),
-  React.createElement("div", { id: "div2" }, [
-    React.createElement("h1", { id: "heading1" }, "Heading 1"),
-    React.createElement("h2", { id: "heading2" }, "Heading 2"),
-  ]),
-]);
+
+const jsxHeading = <h1 id="heading1">Solaxes Technologies by JSX 🔥</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
